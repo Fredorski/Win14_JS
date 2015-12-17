3ec0e947580569c55796794203f4e93168d7db9b
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2015-12-17 09:00:03 +0100

    Datatyper

---
+++ Forelasning_1/Script/index.js
@@ -0,0 +1,8 @@
+﻿alert('hej'); //Crapy but good during development
+//Datatyper:
+var nbr = 12; // Number
+nbr = "Hej"; // dynamiskt typat språk
+var name = "Freddie"; // String
+var myArray = ["Freddie", 5, 'hej', 8.9]; //En array
+var myObject = { name: 'Freddie', age: 38 }; //Ett objekt följer JSON-syntaxen
+var noWay; // undefined, den är namngiven men vet inte vad den är
