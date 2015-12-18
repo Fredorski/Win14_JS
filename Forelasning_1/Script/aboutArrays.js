dac183564256adb94300ea461b9f0625b0667b6c
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2015-12-18 13:55:05 +0100

    send

---
+++ Forelasning_1/Script/aboutArrays.js
@@ -0,0 +1,8 @@
+﻿var array = new Array();
+var array = new Array('hej', 'på', "dig", 'ester');
+var array = ['hej', 'på'];
+array[7] = "I'm out here"; //out of bounce
+array[4] = 5;
+for (var i = 0; i < array.length; i++) {
+    alert(array[i]);//skriver undefined på de element som inte ännu angets
+}
