fc392a4c4f251c084eefa30905e432504b47499b
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2015-12-18 14:29:01 +0100

    StopPropagation :-)

--- Forelasning_2/Scripts/index.js
+++ Forelasning_2/Scripts/index.js
@@ -1,1 +1,19 @@
-﻿
+﻿//Bubling Capturing exempel:
+
+//1)hämtar ut diven med hjälp av dess id:
+var diven = document.getElementById('ett');
+
+//2)lägger till ett klick-event:
+//e är en identifierare för event-objektet
+diven.addEventListener('click', function() {
+    alert('Hej från diven');
+    arguments[0].stopPropagation();
+}, true);
+
+//3) hämtar stycket:
+var stycket = document.getElementById('tvau');
+stycket.addEventListener('click', paragraphFunction, false);
+
+function paragraphFunction() {
+    window.alert('Paragraph is here!');
+}
