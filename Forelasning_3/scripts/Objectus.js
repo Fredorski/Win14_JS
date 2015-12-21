17933c38f576b942772d0fa85df9e53da0d1f20e
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2015-12-21 10:41:45 +0100

    innan lunch

---
+++ Forelasning_3/scripts/Objectus.js
@@ -0,0 +1,27 @@
+﻿//Detta är ett objekt i JS och har JSON-syntax
+
+var kaffemugg =
+{
+    volym: 156,
+    diameter: 12.9,
+    hight: "14",
+    arrayus: [5, 4, 5, "kalle"],
+    drick: function() { alert('Du dricker'); }
+}
+kaffemugg.drick();
+kaffemugg.volyym = 123; //felstavning innebär skapat en ny egenskap på objektet
+
+//Function skapad med constructor notation
+//Kan återanvändas för att skapa flera objekt
+function Dinner(timeToCook, grades, name, duration) {
+    this.timetoCook = timeToCook;
+    this.grades = grades;
+    this.name = name;
+    this.duration = duration;
+    this.ChekHowCoolItIs = function() {
+         return this.grades * this.duration;
+    }
+}
+
+var potatismos = new Dinner(55, 5, "Päremos", 6);
+var spagetti = new Dinner(55, 9, "spagetti", 6);
---
+++ Forelasning_3/scripts/index2.js
@@ -0,0 +1,9 @@
+﻿var hej = 0; //läggs i det globala scopet
+function name() {
+    var hej = 7; // lokalt scoop, är skyddat här inne
+    heja =7+ 21000; //Globala scoopet pga stavfel
+    alert(hej);
+}
+
+name();
+alert(hej);
