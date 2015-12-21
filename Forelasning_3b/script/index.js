a91ef0fa2f1b0c8c9062c87fc4e8e99dd05a9b6c
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2015-12-21 13:16:04 +0100

    jobbade med att skapa element

---
+++ Forelasning_3b/script/index.js
@@ -0,0 +1,14 @@
+﻿//Skapar en <p>-tagg:
+var element = document.createElement('p');
+//Lägger till innehåll i <p>-taggen:
+element.textContent = "Detta är ett stycke";
+//Ändrar bakgrundsfärgen på ett lite fult sätt:
+element.style.backgroundColor = "grey";
+
+//Lägger till klassen gosnalle på <p>-taggen:
+element.setAttribute('class', 'gosnalle');
+
+//För att koppla elementet till HTML-sidan måste vi hämta ut ett befintligt element och lägga till vårt nya:
+
+var diven = document.getElementById('ett');
+diven.appendChild(element);
