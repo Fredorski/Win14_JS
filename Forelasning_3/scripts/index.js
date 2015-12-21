a98b740e3ee922d4f02fb227eefcab13ce0c90cc
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2015-12-21 09:00:30 +0100

    iffy

---
+++ Forelasning_3/scripts/index.js
@@ -0,0 +1,22 @@
+﻿//Bästa sättet att skapa metoder på:
+function namngiven(inparameter) {
+    alert(inparameter);
+    return [5,5,34,25,2,2];
+}
+
+//Function expression finns först när interpreter har funnit funktionen är altså inte garanterad
+var hej = function()
+{
+}
+hej(); //Körs först här 
+
+//IIFE: Immediately invoked function expression 
+//kallas iffy
+//är vanligast för event
+var fun = (function() {
+
+})(); //Körs direkt
+fun(h);
+
+var fun2 = (function () {
+}()); //Körs direkt
