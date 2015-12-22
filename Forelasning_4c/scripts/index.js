7b229914cbe1cef7fe6241d38bee922c2248c767
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2015-12-22 10:52:03 +0100

    JSON-objekt med AJAX i rent JS

---
+++ Forelasning_4c/scripts/index.js
@@ -0,0 +1,23 @@
+﻿var request;
+if (window.XMLHttpRequest) {
+    request = new XMLHttpRequest();
+} else {
+    request = new ActiveXObject("Microsoft.XMLHTTP");
+}
+request.open("GET", "json.json", true);
+request.onreadystatechange = function() {
+    if (request.readyState === 4 && request.status === 200) {
+        var jsonObjectet = JSON.parse(request.responseText);
+        var output = "<ul>";
+        for (var i = 0; i < jsonObjectet.fathers.length; i++) {
+            output += "<li>" +
+                        jsonObjectet.fathers[i].id + " : " +
+                        jsonObjectet.fathers[i].name +
+                        "</li>";
+        }
+        output += "</ul>";
+        document.getElementById('result').innerHTML = output;
+
+    }
+}
+request.send();
