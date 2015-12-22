72a7a78898f2b71ba5438f5ab44bcf985ce3de93
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2015-12-22 09:01:16 +0100

    remove, stoppropagation

---
+++ Forelasning_4/scripts/index.js
@@ -0,0 +1,16 @@
+﻿document.getElementById('ett').addEventListener('click', function (e) {
+    //e.stopPropagation(); //Hindrar framfarten av eventet
+    alert("DIVEN");
+    document.getElementById('tvau').removeEventListener('click', fun, true); //Funkar bara om function är namngiven
+
+    //Om detta eventet tex legat på en knapp
+    //kan vi ta bort knappens standardbeteende 
+    //med följande kod:
+    e.preventDefault(); //förhindrar ett standardbeteende
+}); //True innebär isCapturing / fångar på vägen ner
+
+document.getElementById('tvau').addEventListener('click', fun, true);
+
+function fun() {
+    alert("Paragraph");
+}
