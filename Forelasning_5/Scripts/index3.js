c8007fe363364bd8d7771a5b6d4d325608c100af
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2016-01-04 10:44:33 +0100

    5 finish

--- Forelasning_5/Scripts/index3.js
+++ Forelasning_5/Scripts/index3.js
@@ -10,10 +10,25 @@ var reg2 = new RegExp('tråkig');
 //This and that?
 var text2 = "That is some";
 var thisThat = new RegExp(/th(is|at)/i);
+
+//Växlar till reguljära uttryck:
+// i betyder case insensitive
+// g betyder global, dvs hitta så många som möjligt
+// m betyder multiline och letar på flera rader
+
+//Fler reguljära uttryck:
+// ^ betyder måste starta med
+// $ betyder måste avslutas med
+
+//Upprepning:
 //      /(lisa)*/ matchar även lisalisa, lisalisalisa
 //      /lisa*/ ger lis, lisa, lisaa, lisaaa, lisaaaaa
 //      /lisa+/ ger lisa, lisaa, lisaaa, lisaaaaa
 //      /lisa?/ ger lis, lisa
 //      /lisa{2,3}/ ger lisaa, lisaaa
 
-alert(thisThat.test(text2));
+//alert(thisThat.test(text2));
+// String RegExp metoder:
+alert("hej du glade".search(/glade/)); //hittades på position 7 i textsträngen
+alert("hej du glade".match('hej'));
+alert("hej du glade".replace(/[aeiouåäö]/g, '*'));
