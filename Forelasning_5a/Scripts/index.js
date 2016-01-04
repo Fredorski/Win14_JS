4975787f6fa04e6d19d6df8f6e57fbadc4c344bc
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2016-01-04 11:48:00 +0100

    Lunch

---
+++ Forelasning_5a/Scripts/index.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    //Ändra i ett element
+    //var text = $('h1').text(); //Hämtar texten
+    //$('#ett').text("på julafton äter man samma mat som på påsken");
+    ////Ändra i många element med tag eller klass
+    //var matLista = $('li');
+    //$('li').text('Förändring');
+
+    //2 pooldykningar:
+    //$('#julmat li').text('jul');
+   // $('li:first').text("hej");
+    //Traversing:
+    //  $('li').first().text("hej");//Färre pooldykningar något snabbare kod!
+
+    $('#paskmat li'); //Frestande men 2 pooldyk...
+    $('#paskmat').find('li'); //Sköter allt när vi ändå är på plats 1dyk
+    $('li').first().next(); //Element 2 genom att ta ett steg framåt
+    $('li').last().prev(); //Näst sista elementet
+    //Traversing up in the DOM:
+    $('li').first().parent(); //Gå till föräldern dvs <ul>-elementet
+    //Traversing down in the DOM:
+    //$('#julmat>li');
+    $('#julmat').children('li'); //Hittar bara direkta barn
+    $('#julmat').find('li'); // hittar alla li i #julmat
+});
---
+++ Forelasning_5a/Scripts/index2.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function() {
+    //var element = "Pris: 399kr"; //Bad
+    //var element = "<p> Pris: 399kr </p>"; //bad
+
+    //Skapar ett element men lägger inte till det i DOM-trädet:
+    var element = $("<p> Pris: 399kr </p>"); //good
+    //.append(),   .prepend(),   .after(),       .before()
+    //.appendTo(), .prependTo(), .insertAfter(), .insertBefore();
+    $('#paskmat p:first').prepend(element);
+    $('<span>Hej</span>').appendTo($('#paskmat'));
+    $('button').remove(); //tar bort alla knappar i dom-trädet
+});
