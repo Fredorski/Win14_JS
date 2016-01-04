7f6386b371159ae8ae1c6cea727f0dccfe734d40
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2016-01-04 13:31:05 +0100

    hej

---
+++ Forelasning_5a/Scripts/index3.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function() {
+
+    $('button').on('click', function() {
+        //körs när knappen tryckts:
+        var price = $('<p>39.90Kr/pkt</p>');
+        $(this).after(price);        
+        //$(this).append(price);
+        $(this).remove();
+
+        //Hämta <ul>-elementet:
+        //$(this).parent().parent().append(price);
+        //Hämtar alla föräldrar och farföräldrar osv som är desert
+        //$(this).parents('.desert').append(price);
+        //Bästa sättet att hämta ut, hämtar ur ETT element:
+        $(this).closest('.desert').append(price);
+    });
+
+});
