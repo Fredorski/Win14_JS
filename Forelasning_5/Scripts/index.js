3c26aee98ffccc4a63334e53dc953b11ed3eef91
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2016-01-04 09:44:19 +0100

    for in

---
+++ Forelasning_5/Scripts/index.js
@@ -0,0 +1,30 @@
+﻿//javascript:
+
+var divs = document.getElementsByTagName('div');
+//Ett enda dom-pools-dyk:
+var divLength = divs.length;
+for (var i = 0; divLength > i; i++) {
+    divs[i].style.color = "red";
+}
+
+$('button.send').on('click', function() {
+    $('some').showModalDialog(); //Ett pooldyk
+    $('some').addClass('klasse'); //ett pooldyk
+    $('some').style.color = 'red'; //ett pooldyk
+    /*tänk er att det ligger tre ringar på en bassängs botten och ni ska dyka och hämta dem, men ni hämtar bara en i taget...*/
+});
+//Bättre lösning:
+$('button.better').on('click', function () {
+    $('some')
+        .showModalDialog()
+        .addClass('klasse')
+        .style.color = 'red'; //ett pooldyk
+    /*Vi hämtar alla ringarna i samma dyk...*/
+});
+
+
+//var text = $('#ett').text();
+
+////$('#ett').text('Tjenare');
+//alert(text);
+//$('.hej').text('gurka');
