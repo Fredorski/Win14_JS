892af116dfae0def605ac6eaafaf920ce5eac26b
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2016-01-05 10:05:34 +0100

    All but CSS

--- Forelasning_6/Scripts/index.js
+++ Forelasning_6/Scripts/index.js
@@ -1,16 +1,26 @@
-﻿$(document).ready(function() {
-    $("#search").keyup(function() {
+﻿$(document).ready(function () {
+    $("#search").keyup(function () {
         var searchField = $("#search").val();
         var meExp = new RegExp(searchField, "i");
-        $.getJSON('wt_data.json', function(data) {
+        $.getJSON('wt_data.json', function (data) {
             var output = "<ul class='searchresults'>";
 
-            $.each(data, function(key, val) {
-                output += val.name;
+            $.each(data, function (key, val) {
+                //Dags att sortera:
+                if ((val.name.search(meExp) !== -1) ||
+                    (val.about.search(meExp) !== -1)) {
+                    output += "<li>";
+                    output += "<p>" + val.name + "<b> " + val.master_degree + "</b>" + "</p>";
+                    output += '<img src="wt_images/' + val.url_image;
+                    output += '" alt="' + val.name + '"' + '"/>';
+                    output += "<p>" + val.about + "</p>";
+                    output += "</li>";
+                }
+
             });
             output += "</ul>";
-            $('#update').html(output);
+            $("#update").html(output);
         });
-       
+
     });
 });
