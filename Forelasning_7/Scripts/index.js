d91d887628290fda03af188a3fffe792b8938cae
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2016-01-07 09:31:03 +0100

    jj

--- Forelasning_7/Scripts/index.js
+++ Forelasning_7/Scripts/index.js
@@ -2,79 +2,64 @@
 {
     name: "winnebago",
     type: "big",
-    price: 50,
+    price: 340202,
     make: "Monster",
     model: "T",
     image: "\image/bigCar01.jpg"
-
-
 },
 {
     name: "Air Car",
     type: "big",
-    price: 501,
+    price: 23234,
     make: "Baloon",
     model: "Bad year",
     image: "\image/bigCar02.jpg"
-
-
 },
 {
     name: "hyande",
     type: "medium",
-    price: 1000,
+    price: 12121,
     make: "Hyundaii",
     model: "Accsent",
     image: "\image/mediumCar01.jpg"
-
-
 },
 {
     name: "med2",
     type: "medium",
-    price: 501,
+    price: 43212,
     make: "Ford",
     model: "C#",
     image: "\image/mediumCar02.jpg"
-
-
 },
 {
     name: "med3",
     type: "medium",
-    price: 501,
+    price: 5010,
     make: "Honda",
     model: "Accsent",
     image: "\image/mediumCar03.jpg"
-
-
 },
 {
     name: "small1",
     type: "small",
-    price: 3010,
+    price: 30000,
     make: "Q branch",
     model: "The Sean Connery",
     image: "\image/smallCar01.jpg"
-
 },
 {
     name: "Small2",
     type: "small",
-    price: 501,
+    price: 20000,
     make: "Just Fits",
     model: "Jeremy Clarkson",
     image: "\image/smallCar02.jpg"
-
-
 },
 {
     name: "Small3",
     type: "small",
-    price: 502,
+    price: 18000,
     make: "Just Fits",
     model: "Corp",
-    image: "\image/smallCar03.jpg",
-
-
+    image: "\image/smallCar03.jpg"
 }];
